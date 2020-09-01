97695792863e4a625b74fe6cbbaf89f624790c9f
Tomona Sako <sakotomona@gmail.com>
2020-08-31 23:13:51 -0700

    first commit

---
+++ Lab_OOP/src/example/myapp/Aquarium.kt
@@ -0,0 +1,32 @@
+package example.myapp
+
+open class Aquarium(var length: Int = 100, var width: Int = 20, open var height: Int = 40) {
+
+    open var volume: Int
+        get() = width * height * length / 1000   // 1000 cm^3 = 1 l
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+    init {
+        println("aquarium initializing")
+    }
+
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+
+
+    fun printSize() {
+        println("Width: $width cm " + "Length: $length cm " + "Height: $height cm")
+
+        println("Volume: $volume l Water: $water l (${water/volume*100.0}% full)")
+    }
+}
+
+
